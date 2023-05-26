f10d7e80b3782d537d4978c60d76687208ce3b00
alan910127 <alan469357@gmail.com>
2023-05-26 23:51:39 +0800

    chore: change package prefix to `@rideplus`

--- apps/expo/tailwind.config.ts
+++ apps/expo/tailwind.config.ts
@@ -1,6 +1,6 @@
 import { type Config } from "tailwindcss";
 
-import baseConfig from "@acme/tailwind-config";
+import baseConfig from "@rideplus/tailwind-config";
 
 export default {
   presets: [baseConfig],
