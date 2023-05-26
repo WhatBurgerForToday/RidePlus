35a74fe33dd4931da78397fa25301d5e400012d4
alan910127 <alan469357@gmail.com>
2023-05-26 23:48:17 +0800

    chore: move from golang to ts

--- RidePlus/src/app/oauth-native-callback.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from "react";
-import React from "react";
-import { Redirect } from "expo-router";
-
-const OAuthNativeCallback: FC = () => {
-  return <Redirect href="/home" />;
-};
-
-export default OAuthNativeCallback;
--- server/cmd/server/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/WhatBurgerForToday/server/pkg/server"
-
-func main() {
-	server.Run()
-}
