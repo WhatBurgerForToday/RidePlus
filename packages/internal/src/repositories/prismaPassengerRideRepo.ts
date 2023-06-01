ecfe28b8207c0a367f28d1fd2f1fecc159c1980b
vinchantle <chenvincent610@gmail.com>
2023-06-01 20:17:33 +0800

    feat: implement save for passengerRideRepo

--- packages/internal/src/repositories/prismaPassengerRideRepo.ts
+++ packages/internal/src/repositories/prismaPassengerRideRepo.ts
@@ -1,13 +1,43 @@
-import { type PrismaClient } from "@rideplus/db";
+import { type PrismaClient } from "@prisma/client";
+import { z } from "zod";
 
 import { type PassengerRideRepository } from "~/core/ports/passengerRideRepository";
 
+export const locationsSchema = z.array(
+  z.object({
+    latitude: z.number().min(-90).max(90),
+    longitude: z.number().min(-180).max(180),
+  }),
+);
+
 export const createPrismaPassengerRideRepo = (
-  _prisma: PrismaClient,
+  prisma: PrismaClient,
 ): PassengerRideRepository => {
   return {
-    create: (_ride) => {
-      throw new Error("Not implemented yet");
+    save: async (input) => {
+      const ride = await prisma.ride.upsert({
+        where: {
+          id: input.id,
+        },
+        update: {
+          locations: JSON.stringify(input.locations),
+          status: input.status,
+        },
+        create: {
+          id: input.id,
+          driverId: input.driverId,
+          locations: JSON.stringify(input.locations),
+          status: input.status,
+        },
+      });
+
+      const locations = JSON.parse(ride.locations) as unknown;
+      const parsedLocations = await locationsSchema.parseAsync(locations);
+
+      return {
+        ...ride,
+        locations: parsedLocations,
+      };
     },
   };
 };
