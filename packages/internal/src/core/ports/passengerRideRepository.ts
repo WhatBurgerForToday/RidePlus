c1737ed03b691e4f43db511b8cdfda7610114adc
vinchantle <chenvincent610@gmail.com>
2023-06-01 20:16:42 +0800

    feat: change create to save

--- packages/internal/src/core/ports/passengerRideRepository.ts
+++ packages/internal/src/core/ports/passengerRideRepository.ts
@@ -1,7 +1,14 @@
 import { type PassengerRide } from "~/core/domain/passengerRide";
+import { type Expand } from "~/types/magic";
 
-export type CreatePassengerRideInput = Omit<PassengerRide, "id">;
+export type SaveInput<T extends { id: unknown }> = Expand<
+  Omit<T, "id"> & {
+    id?: T["id"];
+  }
+>;
+
+export type CreatePassengerRideInput = SaveInput<PassengerRide>;
 
 export type PassengerRideRepository = {
-  create: (input: CreatePassengerRideInput) => Promise<void>;
+  save(input: SaveInput<PassengerRide>): Promise<PassengerRide>;
 };
