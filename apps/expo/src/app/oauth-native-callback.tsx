10b32def3c71fc4dfa3db5121557edbe82e3a715
Angela Hsi <applehsi51102@gmail.com>
2023-06-01 22:08:06 +0800

    chore: delete unused login page

--- apps/expo/src/app/oauth-native-callback.tsx
+++
@@ -1,8 +0,0 @@
-import React, { type FC } from "react";
-import { Redirect } from "expo-router";
-
-const OAuthNativeCallback: FC = () => {
-  return <Redirect href="/home" />;
-};
-
-export default OAuthNativeCallback;
