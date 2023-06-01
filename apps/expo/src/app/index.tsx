5835b42db5e14f2ce3e1ede44570996537362a26
alan910127 <alan469357@gmail.com>
2023-06-01 21:45:22 +0800

    feat(api): add api definition

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,20 +1,11 @@
 import React from "react";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useAuth } from "@clerk/clerk-expo";
-
-import { api } from "~/utils/api";
 
 const IndexPage = () => {
-  const { isSignedIn } = useAuth();
-  const sessionQuery = api.auth.getSession.useQuery();
-
-  console.log(isSignedIn, sessionQuery.data);
   return (
     <SafeAreaView className="h-full w-full bg-pink-300">
-      <Text className="bg-pink-400">
-        {JSON.stringify(sessionQuery.data, null, 2)}
-      </Text>
+      <Text className="bg-pink-400"></Text>
     </SafeAreaView>
   );
 };
