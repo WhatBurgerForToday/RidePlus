2e07e3a9c417614615c4ffbe21490749f337202e
vinchantle <chenvincent610@gmail.com>
2023-06-02 17:43:49 +0800

    feat: change to DriverRideStatus

--- packages/internal/src/core/domain/driverRide.ts
+++ packages/internal/src/core/domain/driverRide.ts
@@ -1,5 +1,10 @@
+import { type DriverRideStatus } from "@prisma/client";
+
+import { type Location } from "./location";
+
 export type DriverRide = {
   id: string;
   driverId: string;
   locations: Location[];
+  status: DriverRideStatus;
 };
