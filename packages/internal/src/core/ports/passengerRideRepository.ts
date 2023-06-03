7139a95a47e2a1a77bb2f3734abe32cd1d224c20
vinchantle <chenvincent610@gmail.com>
2023-06-03 22:45:26 +0800

    feat: update input type

--- packages/internal/src/core/ports/passengerRideRepository.ts
+++ packages/internal/src/core/ports/passengerRideRepository.ts
@@ -1,14 +1,11 @@
 import { type PassengerRide } from "~/core/domain/passengerRide";
-import { type Expand } from "~/types/magic";
+import { type MakeOptional } from "~/types/magic";
 
-export type SaveInput<T extends { id: unknown }> = Expand<
-  Omit<T, "id"> & {
-    id?: T["id"];
-  }
+export type SavePassengerRideInput = MakeOptional<
+  PassengerRide,
+  "id" | "status"
 >;
 
-export type CreatePassengerRideInput = SaveInput<PassengerRide>;
-
 export type PassengerRideRepository = {
-  save(input: SaveInput<PassengerRide>): Promise<PassengerRide>;
+  save(input: SavePassengerRideInput): Promise<PassengerRide>;
 };
