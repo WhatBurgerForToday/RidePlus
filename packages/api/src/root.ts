90d6a682fb287d289dc8c544732dab640eb45eaa
BaBa0525 <hsunhsun0525@gmail.com>
2023-06-05 01:28:04 +0800

    chore: remove test code

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,15 +1,10 @@
 import { driverRouter } from "./router/driver";
 import { riderRouter } from "./router/rider";
-import { createTRPCRouter, publicProcedure } from "./trpc";
+import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   rider: riderRouter,
   driver: driverRouter,
-  hello: publicProcedure.query(({ ctx }) => {
-    return {
-      message: ctx.helloService.hello("BaBa"),
-    };
-  }),
 });
 
 // export type definition of API
--- packages/internal/src/core/service/helloService.ts
+++
@@ -1,7 +0,0 @@
-export const createHelloService = () => {
-  return {
-    hello: (name: string) => `Hello ${name}`,
-  };
-};
-
-export type HelloService = ReturnType<typeof createHelloService>;
