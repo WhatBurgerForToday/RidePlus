29d95db6e99d60cdc61924c8872e6a0526865d98
BaBa0525 <hsunhsun0525@gmail.com>
2023-06-06 05:35:31 +0800

    refactor: remove unnecessary component

--- apps/expo/src/components/RatingStars.tsx
+++ apps/expo/src/components/RatingStars.tsx
@@ -9,34 +9,13 @@ export const RatingStars = ({ stars }: RatingStarsProps) => {
   return (
     <View className="mt-1 flex-row">
       {new Array(5).fill(null).map((_, i) => (
-        <StarIcon key={i} isActive={i < Math.round(stars)} />
+        <AntDesign
+          name="star"
+          size={16}
+          backgroundColor="#FFFFFF"
+          color={i < stars ? "#FEC20C" : "#CCCCCC"}
+        />
       ))}
     </View>
-  );
-};
-
-type StarIconProps = {
-  isActive: boolean;
-};
-
-const StarIcon = (props: StarIconProps) => {
-  if (props.isActive) {
-    return (
-      <AntDesign
-        name="star"
-        size={16}
-        backgroundColor="#FFFFFF"
-        color="#FEC20C"
-      />
-    );
-  }
-
-  return (
-    <AntDesign
-      name="star"
-      size={16}
-      backgroundColor="#FFFFFF"
-      color="#CCCCCC"
-    />
   );
 };
