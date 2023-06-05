3d51c7145b15eba085ba831b4d5b9c4d5ee4f18e
alan910127 <alan469357@gmail.com>
2023-06-06 01:55:16 +0800

    feat: add clerk user repository

---
+++ packages/internal/src/core/ports/userRepository.ts
@@ -0,0 +1,6 @@
+import { type User } from "../domain/user";
+
+export type UserRepository = {
+  findById: (id: string) => Promise<User | null>;
+  findManyByIds: (ids: string[]) => Promise<User[]>;
+};
