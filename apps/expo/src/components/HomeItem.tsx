4bcab2b84be9d8c622c6c5c3ed913d803df34857
wayner0628 <a901639@gmail.com>
2023-06-06 13:08:22 +0800

    fix: remove link

--- apps/expo/src/components/HomeItem.tsx
+++ apps/expo/src/components/HomeItem.tsx
@@ -15,36 +15,27 @@ export const HomeItem = (props: HomeItemProps) => {
   const { type, source, destination, departAt } = props;
   return (
     <View className="mx-5 my-2 rounded-lg bg-amber-400 px-2 py-5">
-      <Link
-        href={{
-          pathname: "/",
-        }}
-        asChild
-      >
-        <TouchableOpacity>
-          <View className="flex flex-row">
-            <View className="justify-center px-5">
-              {type === "Recent" && (
-                <AntDesign name="clockcircle" size={24} color="white" />
-              )}
-              {type === "Favorite" && (
-                <FontAwesome name="heart" size={24} color="white" />
-              )}
-            </View>
-            <View className=" px-2">
-              <Text className="py-1 font-bold text-black">
-                FROM ({source.latitude}, {source.longitude})
-              </Text>
-              <Text className="py-1 font-bold text-black">
-                TO ({destination.latitude}, {destination.longitude})
-              </Text>
-              <Text className="py-1 text-white">
-                Depart at {departAt.toDateString()}
-              </Text>
-            </View>
-          </View>
-        </TouchableOpacity>
-      </Link>
+      <View className="flex flex-row">
+        <View className="justify-center px-5">
+          {type === "Recent" && (
+            <AntDesign name="clockcircle" size={24} color="white" />
+          )}
+          {type === "Favorite" && (
+            <FontAwesome name="heart" size={24} color="white" />
+          )}
+        </View>
+        <View className=" px-2">
+          <Text className="py-1 font-bold text-black">
+            FROM ({source.latitude}, {source.longitude})
+          </Text>
+          <Text className="py-1 font-bold text-black">
+            TO ({destination.latitude}, {destination.longitude})
+          </Text>
+          <Text className="py-1 text-white">
+            Depart at {departAt.toDateString()}
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
