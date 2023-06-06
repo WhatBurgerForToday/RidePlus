829ffc445ad7def9915bed8be7eff47c207db2d4
wayner0628 <a901639@gmail.com>
2023-06-06 13:08:46 +0800

    feat: add jump to search

--- apps/expo/src/app/rider/home.tsx
+++ apps/expo/src/app/rider/home.tsx
@@ -1,15 +1,26 @@
 import { FontAwesome } from "@expo/vector-icons";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import React from "react";
-import { Image, Pressable, ScrollView, Text, View } from "react-native";
+import { Image, Pressable, ScrollView, Text, TouchableOpacity, View } from "react-native";
 
 import { HomeItem } from "~/components/HomeItem";
 import { RiderNavbar } from "~/components/Navbar/RiderNavbar";
 import { api } from "~/utils/api";
 
+import {
+  destinationAtom,
+  sourceAtom,
+} from "~/atoms/locationAtom";
+import { dataAtom } from "./search";
+import { useSetAtom } from "jotai";
+
 const HomePage = () => {
   const recentQuery = api.rider.recentRide.useQuery();
   const favoriteQuery = api.rider.favoriteRide.useQuery();
+  const router = useRouter();
+  const setSrcAtom = useSetAtom(sourceAtom);
+  const setDestAtom = useSetAtom(destinationAtom);
+  const setDataAtom = useSetAtom(dataAtom);
 
   return (
     <>
@@ -34,28 +45,32 @@ const HomePage = () => {
         <View className="mx-7 pb-3">
           <Text className="sticky top-0 py-2 text-xl font-bold">Recent</Text>
           {recentQuery.data?.map(({ id, source, destination, departAt }) => (
-            <HomeItem
-              key={id}
-              id={id}
-              type="Recent"
-              source={source}
-              destination={destination}
-              departAt={departAt}
-            />
+            <TouchableOpacity onPress={() => {router.push("/rider/search"); setDataAtom(departAt); setSrcAtom(source); setDestAtom(destination);}}>
+              <HomeItem
+                key={id}
+                id={id}
+                type="Recent"
+                source={source}
+                destination={destination}
+                departAt={departAt}
+              />
+            </TouchableOpacity>
           ))}
         </View>
 
         <View className="mx-7 mb-5 mt-3 pb-3">
           <Text className="py-2 text-xl font-bold">Favorite</Text>
           {favoriteQuery.data?.map(({ id, source, destination, departAt }) => (
-            <HomeItem
-              key={id}
-              id={id}
-              type="Favorite"
-              source={source}
-              destination={destination}
-              departAt={departAt}
-            />
+            <TouchableOpacity onPress={() => {router.push("/rider/search"); setDataAtom(departAt); setSrcAtom(source); setDestAtom(destination);}}>
+              <HomeItem
+                key={id}
+                id={id}
+                type="Recent"
+                source={source}
+                destination={destination}
+                departAt={departAt}
+              />
+            </TouchableOpacity>
           ))}
         </View>
 
