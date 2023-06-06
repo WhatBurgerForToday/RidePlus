0e6b6617d0f9351f594aa0f82d27c016df8b66d9
alan910127 <alan469357@gmail.com>
2023-06-07 01:48:18 +0800

    feat: add response time log for tRPC endpoint

--- apps/server/src/pages/api/trpc/[...trpc].ts
+++ apps/server/src/pages/api/trpc/[...trpc].ts
@@ -1,9 +1,20 @@
+import { type NextApiHandler } from "next";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
 import { appRouter, createTRPCContext } from "@rideplus/api";
 
-// export API handler
-export default createNextApiHandler({
+const tRPCHandler = createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
 });
+
+const handler: NextApiHandler = async (req, res) => {
+  const url = new URL(req?.url ?? "", "http://localhost");
+
+  console.time(url.pathname);
+  const result = await tRPCHandler(req, res);
+  console.timeEnd(url.pathname);
+  return result;
+};
+
+export default handler;
