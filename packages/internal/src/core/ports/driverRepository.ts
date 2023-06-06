45dc1ab8d7ead6a4cb1f513941f0d0a40359877a
vinchantle <chenvincent610@gmail.com>
2023-06-06 16:12:10 +0800

    fix: move findReviews to its own repository

--- packages/internal/src/core/ports/driverRepository.ts
+++ packages/internal/src/core/ports/driverRepository.ts
@@ -2,6 +2,5 @@ import { type Driver } from "../domain/driver";
 
 export type DriverRepository = {
   findById(id: string): Promise<Driver | null>;
-  findReviews(driverId: string): Promise<RideReview[]>;
   save(driver: Driver): Promise<Driver>;
 };
